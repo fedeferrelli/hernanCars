42ed666ecd7b4ea0a1ba7887302368252d459455
Fede <ferrelli.mazza@gmail.com>
2022-01-07 12:01:10 -0300

    maqueteado prueba

--- src/components/Listado/ListadoIndividual.js
+++ src/components/Listado/ListadoIndividual.js
@@ -13,28 +13,36 @@ const PlatoMenu = ({plato}) =>{
          <div className="w-full px-3 mb-4 bg-yellow-500">
             <div className="px-2 md:w-4/12   bg-red-500 rounded-lg h-24 flex justify-center items-center" >
                
-                <div className="flex justify-center items-center w-full bg-slate-600">
+                <div className="flex justify-center items-center m-auto bg-slate-600 h-20 w-full">
 
-                    <div className="w-2/6  lg:w-5/12 xl:w-3/12 md:w-1/4 sm:w-1/3  bg-orange-500"> 
+                    <div className="w-2/6  lg:w-5/12 xl:w-3/12 md:w-1/4 sm:w-1/3 h-full bg-orange-500 flex justify-center items-center" > 
                     
-                        <img src={image} alt="imagen del auto" className="  object-cover rounded-lg w-full "/>
+                        <img src={image} alt="imagen del auto" className=" object-cover rounded-lg w-full h-max-20"/>
 
                     </div>
 
-                    <div className="w-4/6 lg:w-7/12 xl:w-9/12 md:w-3/4 sm:w-2/3 ml-4 bg-white flex flex-col ">
+                    <div className="w-4/6 lg:w-7/12 xl:w-9/12 md:w-3/4 sm:w-2/3  bg-white flex-col  ">
 
-                        <div className="flex bg-red-300">
+                        <div className="pl-2 flex justify-center items-center w-full py-0 px-1 h-10 bg-red-300">
 
-                        <p className="font-bold text-lg text-slate-800 mb-3 w-full">
-                                    {marca}   {modelo}   
+                            <p className="font-bold text-lg text-slate-800 mb-3 w-full">
+                                    {marca}   {modelo} {año}   
                                 </p>
 
-                         </div>
+                        </div>
+
+                        <div className="pl-2 flex w-full py-0 px-1 h-10 bg-green-300">
+
+                            <p className="font-bold text-lg text-slate-800 mb-3 w-full">
+                                        {marca}   {modelo} {año}   
+                                    </p>
+
+                        </div>
 
 
-                         <p className="text-slate-800 mb-4 mt-0 bg-red-500">
+                       {/*   <p className="text-slate-800 mb-4 mt-0 bg-red-500">
                               <span className="font-bold slate-600">{año}</span>                           
-                         </p>
+                         </p> */}
 
                        {/*   <p className="text-gray-600 mb-4  ">
                               {descripcion}                           
