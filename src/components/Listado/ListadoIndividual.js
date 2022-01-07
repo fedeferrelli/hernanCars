7d5253efebcc40d8c3c4156ea68cc87740f89506
Fede <ferrelli.mazza@gmail.com>
2022-01-07 15:26:29 -0300

    maqueteado prueba

--- src/components/Listado/ListadoIndividual.js
+++ src/components/Listado/ListadoIndividual.js
@@ -13,38 +13,55 @@ const PlatoMenu = ({plato}) =>{
          <div className="w-full px-3 mb-4 ">
             <div className="px-2   bg-white  shadow-md  rounded-lg h-24 flex justify-center items-center" >
                
-                <div className="flex justify-center items-center m-auto  h-20 w-full">
+                <div className="flex justify-center items-center h-20 w-full">
 
-                    <div className="w-2/6 lg:w-5/12 xl:w-3/12 md:w-1/4 sm:w-1/3 h-full  flex justify-center items-center " > 
+                   
+                    <div className="w-4/12 lg:w-5/12 xl:w-3/12 md:w-1/4 sm:w-1/3 h-full  flex justify-center items-center " > 
                     
                         <img src={image} alt="imagen del auto" className=" object-cover rounded-lg w-full h-max-20"/>
 
                     </div>
 
-                    <div className="w-4/6 lg:w-7/12 xl:w-9/12 md:w-3/4 sm:w-2/3  bg-white flex-col  ">
+                   
+                   
+                    <div className="w-6/12 lg:w-7/12 xl:w-9/12 md:w-3/4 sm:w-2/3  bg-white flex-col  ">
 
-                        <div className="pl-2 flex justify-center items-center w-full py-0 px-1 h-10 ">
+                        <div className="pl-2 flex-col justify-center items-center w-full py-0 px-1 ">
 
-                            <p className="font-bold text-2xl text text-slate-800 mb-3 w-full">
-                                    {marca} {modelo} {año}   
-                                </p>
-
-                        </div>
-
-                        <div className="pl-2 flex  items-center w-full py-0 px-1 h-10 ">
-
-                        <a href='https://api.whatsapp.com/send?phone=5492915033111&text=Hola,%20Hernán.%20Cómo%20estás?%20Me%20gustaría%20hecerte%20la%20siguiente%20consulta.%0D%0A%0D%0A'>{<FaWhatsapp className="text-2xl text-green-800 bold"/>}</a>
-                        
-
-                        <a href="tel:+5492262553135">{<FaPhoneAlt className="text-2xl text-gray-600 ml-4 "/>}</a>
-
+                            <p className="font-bold text-2xl text text-slate-800 mb-0 w-full">
+                                {marca} {modelo}   
+                            </p>
+                                
+                            <p className="text-lg text text-slate-800 mt-0 mb-0 w-full">
+                                {año}   
+                            </p>
 
                         </div>
+
+
 
                                         
                     </div>
 
                
+                    <div className="w-2/12 lg:w-5/12 xl:w-3/12 md:w-1/4 sm:w-1/3 h-full flex flex-col items-center justify-around" > 
+                    
+                    
+
+                        <a href='https://api.whatsapp.com/send?phone=5492915033111&text=Hola,%20Hernán.%20Cómo%20estás?%20Me%20gustaría%20hecerte%20la%20siguiente%20consulta.%0D%0A%0D%0A'>{<FaWhatsapp className="text-2xl text-green-800 bold"/>}</a>
+
+
+                        <a href="tel:+5492262553135">{<FaPhoneAlt className="text-2xl text-gray-600 "/>}</a>
+
+
+                    
+                    </div>
+
+
+
+
+
+
 
                 </div>
             
