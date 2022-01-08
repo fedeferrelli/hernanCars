2af271a7bc08f87a643984b376071cb618b47eac
Fede <ferrelli.mazza@gmail.com>
2022-01-08 18:34:28 -0300

    pantalla detalle.

--- src/components/Listado/Detalle.js
+++ src/components/Listado/Detalle.js
@@ -35,7 +35,7 @@ const Detalle = ({plato}) =>{
       <h3 className="font-sans px-4 text-lg text-center text-slate-700  mb-5 w-full" > {descripcion}</h3>
 
       {/* <button>{<FaAngleLeft className=" bg-yellow-500 rounded-full text-4xl text-grey-500 p-2 bottom-10 right-10  absolute"/>}</button> */}
-      <Link to="/otra" state={{ from: plato }}>{<FaAngleLeft className=" bg-yellow-500 rounded-full text-4xl text-grey-500 p-2 bottom-10 right-10  absolute"/>}</Link>
+      <Link to="/otra" state={{ from: plato }}>{<FaAngleLeft className=" bg-yellow-500 rounded-full text-4xl text-grey-500 p-2 bottom-20 right-10  absolute"/>}</Link>
 
       </div>
     )
