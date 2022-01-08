67431dd236dbaba709fdfbd282a3ac9d5f7df439
Fede <ferrelli.mazza@gmail.com>
2022-01-08 16:01:58 -0300

    pantalla detalle.

--- src/components/Listado/ListadoIndividual.js
+++ src/components/Listado/ListadoIndividual.js
@@ -12,21 +12,22 @@ const PlatoMenu = ({plato}) =>{
     return(
 
 
-         <div className="w-full px-3 mb-4 ">
-            <div className="px-2   bg-white  shadow-md  rounded-lg h-28 flex justify-center items-center " >
+         <div className="w-full px-2 mb-4 ">
+            
+            <div className="px-1   bg-white  shadow-md  rounded-lg h-28 flex justify-center items-center " >
                
-                <div className="flex justify-center items-center h-28 w-full">
+                <div className="flex justify-between items-center h-28 w-full">
 
                    
-                    <div className="w-4/12 lg:w-5/12 xl:w-3/12 md:w-1/4 sm:w-1/3 h-full  flex justify-center items-center " > 
+                    <div className="w-4/12 h-full  flex justify-center items-center " > 
                     
-                        <img src={image} alt="imagen del auto" className=" object-cover rounded-lg w-full "/>
+                        <img src={image} alt="imagen del auto" className=" object-cover rounded-lg w-full h-20 "/>
 
                     </div>
 
                    
                    
-                    <div className="w-8/12 lg:w-7/12 xl:w-9/12 md:w-3/4 sm:w-2/3  bg-white flex-col  ">
+                    <div className="w-7/12 lg:w-7/12 xl:w-9/12 md:w-3/4 sm:w-2/3  bg-white flex-col  ">
 
                         <div className="pl-2 flex-col justify-between items-center w-full py-0 px-1 ">
 
@@ -44,50 +45,22 @@ const PlatoMenu = ({plato}) =>{
                            
                             <div className=" flex items-center" > 
 
-                                    <a href='https://api.whatsapp.com/send?phone=5492915033111&text=Hola,%20Hernán.%20Cómo%20estás?%20Me%20gustaría%20hecerte%20la%20siguiente%20consulta.%0D%0A%0D%0A'>{<FaWhatsappSquare className="text-xl text-green-800 rounded-xl"/>}</a>
+                                    <a href='https://api.whatsapp.com/send?phone=5492915033111&text=Hola,%20Hernán.%20Cómo%20estás?%20Me%20gustaría%20hecerte%20la%20siguiente%20consulta.%0D%0A%0D%0A'>{<FaWhatsappSquare className="text-xl text-green-400 rounded-xl"/>}</a>
 
 
-                                    <a href="tel:+5492262553135">{<FaPhoneSquareAlt className="text-xl text-gray-400 rounded-xl ml-3"/>}</a>
+                                    <a href="tel:+5492262553135">{<FaPhoneSquareAlt className="text-xl text-blue-400 rounded-xl ml-3"/>}</a>
  
                             </div>
 
                         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                        
+                  
                     </div>
 
-               
-      {/*               <div className="w-2/12 lg:w-5/12 xl:w-3/12 md:w-1/4 sm:w-1/3 h-full flex flex-col items-center justify-around" > 
-                    
-                    
+                    <div className="w-1/12 lg:w-7/12 xl:w-9/12 md:w-3/4 sm:w-2/3">
 
-                        <a href='https://api.whatsapp.com/send?phone=5492915033111&text=Hola,%20Hernán.%20Cómo%20estás?%20Me%20gustaría%20hecerte%20la%20siguiente%20consulta.%0D%0A%0D%0A'>{<FaWhatsappSquare className="text-xl text-green-800 rounded-xl"/>}</a>
+                         <Link to="/detalle" state={{ from: plato }}>{<FaAngleRight className="text-2xl text-slate-400 bold rounded-xl"/>}</Link>
 
-
-                        <a href="tel:+5492262553135">{<FaPhoneSquareAlt className="text-xl text-gray-400 rounded-xl"/>}</a>
-
-
-                    
-                    </div> */}
-
-
-                   <Link to="/detalle" state={{ from: plato }}>{<FaAngleRight className="text-2xl text-slate-400 bold rounded-xl"/>}</Link>
-
-
+                   </div>
 
 
 
