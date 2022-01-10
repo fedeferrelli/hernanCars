90f03c6dd3d2bbcb2550867f6c0cac9035d5aea0
Fede <ferrelli.mazza@gmail.com>
2022-01-10 10:22:37 -0300

    pantalla detalle.

--- src/components/Listado/Detalle.js
+++ src/components/Listado/Detalle.js
@@ -18,24 +18,35 @@ const Detalle = ({plato}) =>{
 
 <div className=" min-h-screen items-center flex flex-col relative bg-neutral-200">
 
-      <img src={image} alt="imagen del auto" className=" object-cover w-full h-max-20"/>
-          <div className="flex items-center justify-around mt-1 w-1/2" > 
+      <img src={image} alt="imagen del auto" className=" object-cover rounded-md mt-4 w-11/12"/>
+      
+      <div className="flex items-center justify-around w-1/2 mt-5" > 
 
-          <a href='https://api.whatsapp.com/send?phone=5492262574254&text=Hola,%20Hernán.%20Cómo%20estás?%20Me%20gustaría%20hecerte%20la%20siguiente%20consulta.%0D%0A%0D%0A'>{<FaWhatsappSquare className="text-5xl text-green-800 rounded-xl"/>}</a>
+      <div className="bg-green-800 p-1.5 rounded-full">
+         
+          <a href='https://api.whatsapp.com/send?phone=5492262574254&text=Hola,%20Hernán.%20Cómo%20estás?%20Me%20gustaría%20hecerte%20la%20siguiente%20consulta.%0D%0A%0D%0A'>{<FaWhatsapp className="text-4xl text-green-100  bg-green-800 rounded-xl p-1"/>}</a>
+         </div>
 
 
-          <a href="tel:+5492262574254">{<FaPhoneSquareAlt className=" text-5xl text-blue-500  rounded-xl"/>}</a>
+        <div className="bg-orange-500 rounded-full">
+          <a href="tel:+5492262574254">{<FaPhoneAlt className=" p-3 text-5xl text-white   rounded-full"/>}</a>
+          </div>
+
 
       </div>
+
+      <div className="bg-white rounded-md w-11/12 px-3 py-4 mt-5">
     
-      <h1 className="font-sans font-bold text-3xl text-center text-slate-800 mt-5 mb-0 w-full">{marca} {modelo} </h1>
+      <h1 className="font-sans font-bold text-3xl text-center text-slate-800  mb-0 w-full">{marca} {modelo} </h1>
       
       <h2 className=" font-sans text-xl text-center text-slate-700  mb-5 w-full"> {año} </h2>
       
       <h3 className="font-sans px-4 text-lg text-center text-slate-700  mb-5 w-full" > {descripcion}</h3>
+      </div>
+     
+      <Link to="/otra" state={{ from: plato }}>{<FaArrowLeft className=" bg-yellow-500 rounded-full text-5xl text-gray-500 p-4  bottom-15 right-10  absolute"/>}</Link>
 
-      {/* <button>{<FaAngleLeft className=" bg-yellow-500 rounded-full text-4xl text-grey-500 p-2 bottom-10 right-10  absolute"/>}</button> */}
-      <Link to="/otra" state={{ from: plato }}>{<FaArrowLeft className=" bg-yellow-500 rounded-full text-5xl text-gray-500 p-4  bottom-20 right-10  absolute"/>}</Link>
+      
 
       </div>
     )
